1008d3b5a756a886ed450f160149346a2eb886fb
Faysal Hasan <faysalhasan950@gmail.com>
2025-08-10 17:49:34 +0600

    changed card and newsletter color

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -9,7 +9,7 @@ const NewsLetter = () => {
             <h1 className='text-3xl font-bold text-gray-800'>Subscribe Newsletter</h1>
             <h2 className='font-gray-400 w-1/3 text-center'>Never miss our latest updates, podcasts etc. Our newsletter is once a week, every Friday. </h2>
             <div className='mt-3'>
-                <input className='bg-white w-[400px] h-[50px] rounded-tl-3xl rounded-bl-3xl pl-6 placeholder:text-xl' placeholder='user@email.com' type="text" />
+                <input className='bg-white outline outline-gray-300 w-[400px] h-[50px] placeholder:text-gray-400 rounded-tl-3xl rounded-bl-3xl pl-6 placeholder:text-xl' placeholder='user@email.com' type="text" />
                 <button className='h-[50px] px-4 bg-[#15c39a] rounded-br-3xl cursor-pointer rounded-tr-3xl text-white font-semibold'>Subscribe</button>
             </div>
             <p className='text-gray-400 text-xl mb-10 mt-4'>We promise not to spam you!</p>
